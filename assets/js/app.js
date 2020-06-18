a7dd96b1865f1973c1c986ff994f2a6414127c24
Tim Sherry <tsherry@tjs-mbp-2.home>
2020-06-18 14:26:15 -0400

    added styling and icons to list

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,10 @@ $('input[type = "text"]').keypress(function (e) {
     if (e.which === 13) {
         const newTodo = ($(this).val());
         $(this).val('')
-        $('ul').append('<li><span>X </span>' + newTodo + '</li>')
+        $('ul').append('<li><span><i class="fa fa-trash"></i> </span>' + newTodo + '</li>')
     }
+})
+
+$('.fa-plus').click(function() {
+    $('input[type = "text"]').fadeToggle();
 })
